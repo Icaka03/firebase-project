235d4947313bbeb94db9911a398784d8bbd6c55d
Hristo <generatorfox@gmail.com>
2024-01-25 12:06:25 +0200

    conect to firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
 import "./App.css";
-
+import Main from "./pages/main";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDq2_GqlSoSYSfQWOl9qOYE9EBGmXeuM3M",
+  authDomain: "fir-project-78c29.firebaseapp.com",
+  projectId: "fir-project-78c29",
+  storageBucket: "fir-project-78c29.appspot.com",
+  messagingSenderId: "97214223978",
+  appId: "1:97214223978:web:6fd5d29c096d49246c9c13",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
